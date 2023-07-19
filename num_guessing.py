90b5159e367d60ec904bdefa26a288ec6d2b8598
Vantasy <xxvantasy@gmail.com>
2023-07-19 19:29:21 +0700

    First Commit

---
+++ num_guessing.py
@@ -0,0 +1,25 @@
+import time
+import random
+
+print("")
+print("Number Guessing\n")
+time.sleep(1)
+
+while True:
+    valid = int(input("Press 1 for ready! "))
+    if valid == 1:
+        time.sleep(0.5)
+        num = random.randrange(1, 10)
+        while True:
+            num_guess = int(input("Any Guess? "))
+            if num_guess == num:
+                time.sleep(0.75)
+                print("Wow, you're so good at guessing!")
+                break
+            else: 
+                time.sleep(0.75)
+                print("Try again!\n")
+        break
+    else:
+        time.sleep(0.5)
+        print("You have to be ready!\n")
---
+++ simple_calculation.py
@@ -0,0 +1,35 @@
+import time
+
+print("")
+print("QUIZZ!\n")
+time.sleep(1)
+
+print("1. What's the result of 5 + 5?")
+ans_1 = int(input("Input your answer: "))
+time.sleep(0.75)
+if ans_1 == 10:
+    print("You're correct!\n")
+    ans_1 = 1
+else:
+    print("You're incorrect!\n")
+    ans_1 = 0
+
+print("2. What's the x of 4 < x < 7? ")
+ans_2 = int(input("Input your answer: "))
+time.sleep(0.75)
+if ans_2 == 5 or 6:
+    print("You're correct!\n")
+    ans_2 = 1
+else:
+    print("You're incorrect!\n")
+    ans_2 = 1
+
+time.sleep(0.75)
+
+ans = ans_1 + ans_2
+if ans == 2:
+    print("You're so good at this!")
+elif ans == 1:
+    print("Nice Try!")
+else:
+    print("It's okay!")
